600cfe6ab7ee99a960021674c543e5838721827a
ultalex <82980736+ultalex@users.noreply.github.com>
2024-03-02 02:16:06 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+let block = document.querySelector('.block')
+let create_card_btn = document.querySelector('body')
+
+create_card_btn.addEventListener('click', function(e) {
+   block.style.transform = `translate(${e.pageX - 25}px, ${e.pageY - 25}px)`
+   block.style.opacity = 1
+   anime({
+       targets: '.block',
+       opacity: 0,
+       scale: 2,
+       display: 'none',
+       duration: 2000
+   })
+})
